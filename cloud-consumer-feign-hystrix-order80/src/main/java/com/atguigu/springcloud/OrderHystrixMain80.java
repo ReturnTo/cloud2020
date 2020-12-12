3f0a7d51d3b62295e1c84fa018065a23a54ff051
ReturnTo <2541360148@qq.com>
2020-12-12 16:15:05 +0800

    12-12-Hystrix完成学习

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/OrderHystrixMain80.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;/*
+ * @Author 裴舒凯
+ * @Date 2020-11-29 15:49
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableHystrix
+public class OrderHystrixMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderHystrixMain80.class,args);
+    }
+}
---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/atguigu/springcloud/service/PaymentFallbackService.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud.service;/*
+ * @Author 裴舒凯
+ * @Date 2020-12-02 21:33
+ */
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentFallbackService implements PaymentHystrixService{
+    @Override
+    public String paymentInfo_OK(Integer id) {
+        return "-----PaymentFallbackService fall back-paymentInfo_OK , (┬＿┬)";
+    }
+
+    @Override
+    public String paymentInfo_TimeOut(Integer id) {
+        return "-----PaymentFallbackService fall back-paymentInfo_TimeOut , (┬＿┬)";
+    }
+}
